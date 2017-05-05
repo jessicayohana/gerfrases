2f6cdf8238ad3937d319543f204de5d5d3d02fee
jessicayohana <jessicayohanaotto@gmail.com>
2017-05-05 09:30:17 -0300

    upload da aplicação

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php require 'logica.php';?>
+
+    <!doctype html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+
+       <h2> <?= "Olá $tratamento $nome, $qualidade $lugar" ?></h2>
+
+
+</body>
+</html>
---
+++ logica.php
@@ -0,0 +1,11 @@
+<?php
+
+$tratamentos = ['Dom', 'samurai','oraculo'];
+$qualidades  = ['o paladino', 'o colosso','o homosapiens'];
+$lugares     = ['das terras medias','de araquari','de hogwarts'];
+$nome       = "Maria";
+
+$tratamento = $tratamentos[rand(0,2)];
+$qualidade  = $qualidades[rand(0,2)];
+$lugar      = $lugares[rand(0,2)];
+?>
